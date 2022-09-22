5fbcf9615ab71e4bebd0d40cb49594192a2bb7bb
deepak <deepakkumarn086747@gmail.com>
2022-09-22 08:43:56 -0500

    feat: cors

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const adminseed = require('./seeder/seedAdmin')
 const path = require('path');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swegger.json');
+const cors = require('cors')
 
 
 const error = require('./utils/validation/errorHandle');
@@ -18,6 +19,8 @@ app.use(express.urlencoded({extended:false, limit:'10mb'}));
 
 app.use(express.static(path.join(__dirname, 'images')))
 app.use(express.static(path.join(__dirname, 'resume')))
+
+app.use(cors())
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
